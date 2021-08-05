e537a5bba130f7a3110e7f6e639feb84f0e44dc0
Rabia-Rajput <rabiarajput0300@gmail.com>
2021-08-05 19:33:57 +0500

    Menus in android studio

---
+++ app/src/main/java/com/firstapp/menus/MainActivity.java
@@ -0,0 +1,183 @@
+package com.firstapp.menus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    MenuItem item;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item_1:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_2:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_3:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_4:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_5:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_6:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_7:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_8:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_9:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_10:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_11:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_12:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_13:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_14:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_15:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_16:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_17:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_18:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_19:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_20:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_21:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_22:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_23:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_24:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_25:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_26:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_27:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_28:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_29:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_30:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_31:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_32:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_33:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_34:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_35:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_36:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_37:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_38:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_39:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_40:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_41:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_42:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_43:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_44:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_45:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_46:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_47:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_48:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_49:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_50:
+                Toast.makeText(getApplicationContext(),item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+}
